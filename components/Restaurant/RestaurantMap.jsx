c49666a6217200fc25b5a54fbaf4eb72c5a48c93
Paul MAZEAU <mazeau.paul@gmail.com>
2023-10-22 17:17:43 +0200

    Display map

--- components/Restaurant/RestaurantMap.jsx
+++ components/Restaurant/RestaurantMap.jsx
@@ -1,20 +1,64 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { main } from '../../constants/color';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, TouchableOpacity, Image, Alert } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { useNavigation } from '@react-navigation/native';
+import * as Location from 'expo-location';
 
-export default function RestaurantMap() {
-    return (
-        <View style={styles.container}>
-        </View>
-    )
-}
+const RestaurantMap = () => {
+  const navigation = useNavigation();
+  const [mapRegion, setMapRegion] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert("Permission refusée", "L'accès à la localisation est nécessaire pour cette fonctionnalité.");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setMapRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    })();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+
+      {mapRegion && (
+        <MapView
+          style={{ alignSelf: 'stretch', height: '100%' }}
+          region={mapRegion}
+        >
+          <Marker coordinate={mapRegion} />
+        </MapView>
+      )}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        borderWidth: 1,
-        borderColor: 'black',
-        backgroundColor: main.LogoPink,
-        height: 260,
-        borderRadius: 14
-    },
-})
+  container: {
+    flex: 1,
+    borderRadius: 14,
+    overflow: 'hidden',
+    borderWidth: 1,
+    borderColor: 'blacks'
+  },
+  backButton: {
+    position: 'absolute',
+    top: 50,
+    left: 14,
+    zIndex: 5, 
+  },
+  backIcon: {
+    width: 35, 
+    height: 35
+  }
+});
+
+export default RestaurantMap;
