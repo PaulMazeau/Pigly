79c64cafd33ac862534733b37df68e29aac1b7a3
white-duke <identifiantapple.ipad6@gmail.com>
2024-01-05 23:47:27 +0100

    La j'espere c bon

--- context/userContext.js
+++ context/userContext.js
@@ -1,36 +1,39 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
 import * as Location from 'expo-location';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, onSnapshot } from 'firebase/firestore';
 import { FB_DB } from '../firebaseconfig';
 import { useAuth } from './AuthContext';
 
 const UserContext = createContext();
-
 export const useUser = () => useContext(UserContext);
-
 export const UserProvider = ({ children }) => {
   const { currentUser } = useAuth();
   const [profile, setProfile] = useState({});
   const [location, setLocation] = useState(null);
 
   useEffect(() => {
-    const fetchProfile = async () => {
-      if (currentUser) {
-        const userProfileRef = doc(FB_DB, 'users', currentUser.uid);
-        console.log("currentUser.uid :",currentUser.uid);
-        const userProfileSnap = await getDoc(userProfileRef);
-        if (userProfileSnap.exists()) {
+    let unsubscribeFromUser = () => {};
+
+    if (currentUser) {
+      const userProfileRef = doc(FB_DB, 'users', currentUser.uid);
+
+      // S'abonner aux mises à jour du document utilisateur
+      unsubscribeFromUser = onSnapshot(userProfileRef, (doc) => {
+        if (doc.exists()) {
           setProfile({
-            ...userProfileSnap.data(),
+            ...doc.data(),
             uid: currentUser.uid
           });
+        } else {
+          console.log("No such user!");
         }
-      }
-    };
+      });
+    }
 
-    fetchProfile();
+    return () => {
+      unsubscribeFromUser(); // Nettoyer l'abonnement lors du démontage du composant
+    };
   }, [currentUser]);
-  console.log("Profile :",profile);
 
   useEffect(() => {
     (async () => {
@@ -39,17 +42,14 @@ export const UserProvider = ({ children }) => {
         console.error('Permission to access location was denied');
         return;
       }
-
       let userLocation = await Location.getCurrentPositionAsync({});
       setLocation(userLocation);
     })();
   }, []);
-
   const value = {
     profile,
     location,
   };
-
   return (
     <UserContext.Provider value={value}>
       {children}
