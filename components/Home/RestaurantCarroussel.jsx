1796952d07b875ce4885b0bfac90763835b0532e
Paul MAZEAU <mazeau.paul@gmail.com>
2024-01-07 18:48:27 +0100

    enlever la pagination

--- components/Home/RestaurantCarroussel.jsx
+++ components/Home/RestaurantCarroussel.jsx
@@ -2,10 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { View, Dimensions } from 'react-native';
 import { collection, getDocs } from "firebase/firestore";
 import { FB_DB } from '../../firebaseconfig';
-import Animated, { Extrapolate, interpolate, useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
+import { useSharedValue } from 'react-native-reanimated';
 import Carousel from 'react-native-reanimated-carousel';
 import ImageCarroussel from './ImageCarroussel';
-import { main } from '../../constants/color';
 
 const PAGE_WIDTH = Dimensions.get('window').width;
 
@@ -45,75 +44,6 @@ function RestaurantCarroussel() {
         }}
         data={restaurants}
         renderItem={({ item }) => <ImageCarroussel restaurant={item} />}
-      />
-
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "center",
-          marginTop: 10, // Adjust as needed
-        }}
-      >
-        {restaurants.map((_, index) => (
-          <PaginationItem
-            key={index}
-            animValue={progressValue}
-            index={index}
-            length={restaurants.length}
-          />
-        ))}
-      </View>
-    </View>
-  );
-}
-
-function PaginationItem(props) {
-  const { animValue, index, length } = props;
-  const width = 10;
-
-  const animStyle = useAnimatedStyle(() => {
-    let inputRange = [index - 1, index, index + 1];
-    let outputRange = [-width, 0, width];
-
-    if (index === 0 && animValue.value > length - 1) {
-      inputRange = [length - 1, length, length + 1];
-      outputRange = [-width, 0, width];
-    }
-
-    return {
-      transform: [
-        {
-          translateX: interpolate(
-            animValue.value,
-            inputRange,
-            outputRange,
-            Extrapolate.CLAMP,
-          ),
-        },
-      ],
-    };
-  }, [animValue, index, length]);
-
-  return (
-    <View
-      style={{
-        backgroundColor: 'gray',
-        width,
-        height: width,
-        borderRadius: 50,
-        overflow: 'hidden',
-        marginHorizontal: 2
-      }}
-    >
-      <Animated.View
-        style={[
-          {
-            borderRadius: 50,
-            backgroundColor: main.LogoBlack, // La couleur du point
-            flex: 1,
-          },
-          animStyle,
-        ]}
       />
     </View>
   );
