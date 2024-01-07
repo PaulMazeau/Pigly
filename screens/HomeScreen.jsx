b2087c28177151cb7ea057b493c7b96cb81535e2
Paul MAZEAU <mazeau.paul@gmail.com>
2024-01-07 18:19:39 +0100

    Relier le carroussel du menu

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    paddingHorizontal: 14,
+    padding: 14,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'white'
--- screens/Map.jsx
+++ screens/Map.jsx
@@ -1,13 +1,15 @@
 import React, { useContext } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import RestaurantContext from '../context/RestaurantContext';
 import RestaurantCard from '../components/Home/RestaurantCard';
+import Header from '../components/Reusable/Header';
 
 export default function Map({ navigation }) {
   const { restaurants } = useContext(RestaurantContext);
   console.log("Data des restaurants pour Map :",{ restaurants });
   return (
     <View style={styles.container}>
+      <Header/>
       <Text style={styles.subTitle}>Tous nos restaurants</Text>
         {restaurants.map(restaurant => (
           <RestaurantCard key={restaurant.id} restaurant={restaurant} />
@@ -20,13 +22,8 @@ export default function Map({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 14,
     backgroundColor: '#F5F5F5',
-  },
-  containerCarrousel:{
-    flexDirection: 'row',
-    paddingBottom: 500
   },
   title: {
     fontSize: 24,
