1f8f864ad229d675d6370da60ecc096cf2fbd991
Paul MAZEAU <mazeau.paul@gmail.com>
2024-01-07 13:53:03 +0100

    Changement font header

--- components/Reusable/Header.jsx
+++ components/Reusable/Header.jsx
@@ -1,28 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import MoodTag from './MoodTag'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import MoodTag from './MoodTag';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import * as Font from 'expo-font';
+import { main } from '../../constants/color';
 
 export default function Header() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  async function loadFonts() {
+    await Font.loadAsync({
+      'Thunder-BlackLC': require('../../assets/fonts/Thunder-BlackLC.otf'),
+    });
+    setFontsLoaded(true);
+  }
+
+  useEffect(() => {
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return <View/>;
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.logo}>Pigly</Text>
       <MoodTag/>
     </SafeAreaView>
-   )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#00000000', 
     paddingBottom: Platform.OS === 'android' ? 25 : -25,
-    marginTop: 36,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     width: '100%',
   },
   logo: {
-    fontSize: 20
-  }
-})
+    fontSize: 40,
+    fontFamily: 'Thunder-BlackLC',
+    color: main.LogoPink,
+  },
+});
